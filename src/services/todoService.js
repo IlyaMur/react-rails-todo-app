4b4b84445dfa7c991677ff31318a10e62bc55436
Ilya Mur <ilyamur@hotmail.com>
2021-10-07 16:43:24 +0300

    add base file structure + set toDoService

---
+++ src/services/todoService.js
@@ -0,0 +1,19 @@
+const baseUrl = 'http://127.0.0.1:3000/todos';
+
+export const loadTodos = () => {
+  return fetch(baseUrl).then((res) => res.json());
+};
+
+export const getTodo = (id) => {
+  return fetch(`${baseUrl}/${id}`).then((res) => res.json());
+};
+
+export const createTodo = (todo) => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title: todo.title, completed: todo.completed }),
+  });
+};
